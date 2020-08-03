bccaf531c0b109ab1743059b25a28effcbc71633
osman forhad <osmanforhad@pop-os.localdomain>
2020-08-03 20:45:55 +0600

    making Quick action icon Dynamic and usable

--- app/src/main/java/com/example/tipsglobal/activities/CreateNoteActivity.java
+++ app/src/main/java/com/example/tipsglobal/activities/CreateNoteActivity.java
@@ -140,6 +140,21 @@ public class CreateNoteActivity extends AppCompatActivity {
 
         });//end of the setOnClickListener
 
+        if (getIntent().getBooleanExtra("isFromQuickActions", false)) {
+            String type = getIntent().getStringExtra("quickActionType");
+            if (type != null) {
+                if (type.equals("image")) {
+                    selectedImagePath = getIntent().getStringExtra("imagePath");
+                    imageNote.setImageBitmap(BitmapFactory.decodeFile(selectedImagePath));
+                    imageNote.setVisibility(View.VISIBLE);
+                    findViewById(R.id.imageRemoveImage).setVisibility(View.VISIBLE);
+                } else if (type.equals("URL")) {
+                    textWebURL.setText(getIntent().getStringExtra("URL"));
+                    layoutWebURL.setVisibility(View.VISIBLE);
+                }
+            }
+        }
+
         initMiscellaneous();//calling the method
         setSubtitleIndicatorColor();//calling the method
 
